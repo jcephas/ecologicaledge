631278a4fb67c1b7c0216725c967d7f8ce0b71f8
jcephas <jcephas@gmail.com>
2021-04-29 09:03:50 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,29 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<?php 
 
-    <title>ARCH/SUEN 7140</title>
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-</head>
+include_once "common/header.php";
 
-<style>
-	img {
-		width:50%;
-	}
+?>
 
-</style>
+<h1>The Ecological Edge: Landscape Dynamics, Social Justice, and Urban Instability</h1>
 
-<body>
+<div class="section">
+   <img src="img/constitutionbeach.jpg">
+   <h4><a href="01-projects/Alessandro/index.html">Alessandro</a></h4>
+   <h4><a href="01-projects/Katerie/index.html">Katerie</a></h4>
+   <h4><a href="01-projects/Kyle/index.html">Kyle</a></h4>
+   <h4><a href="01-projects/Laura/index.html">Laura</a></h4>
+   <h4><a href="01-projects/Patrick/index.html">Patrick</a></h4>
+   <h4><a href="01-projects/Steph/index.html">Steph</a></h4>
+   <h4><a href="01-projects/Taylor/index.html">Taylor</a></h4>
+   <h4><a href="01-projects/Zeina/index.html">Zeina</a></h4>
+</div>
 
-    <h1>The Ecological Edge: Landscape Dynamics, Social Justice, and Urban Instability</h1>
-    <img src="img/constitutionbeach.jpg">
-    <h4><a href="01-projects/Alessandro/index.html">Alessandro</a></h4>
-    <h4><a href="01-projects/Katerie/index.html">Katerie</a></h4>
-    <h4><a href="01-projects/Kyle/index.html">Kyle</a></h4>
-    <h4><a href="01-projects/Laura/index.html">Laura</a></h4>
-    <h4><a href="01-projects/Patrick/index.html">Patrick</a></h4>
-    <h4><a href="01-projects/Steph/index.html">Steph</a></h4>
-    <h4><a href="01-projects/Taylor/index.html">Taylor</a></h4>
-    <h4><a href="01-projects/Zeina/index.html">Zeina</a></h4>
+<?php 
 
-</body>
+include_once "common/footer.php";
 
-</html>
+?>
